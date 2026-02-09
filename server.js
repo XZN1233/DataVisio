ed689b8466432c76e3acfd01f38d105b229af2ae
znxiao <znxiao@coremail.cn>
2026-02-09 09:47:44 +0800

    refactor: 将 require 语句替换为 import 语法
    将 CommonJS 的 require 语法更新为 ES Module 的 import 语法，以保持代码一致性并支持现代 JavaScript 特性

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql2/promise');
-const Redis = require('ioredis');
+import express from 'express';
+import cors from 'cors';
+import mysql from 'mysql2/promise';
+import Redis from 'ioredis';
 
 const app = express();
 const PORT = 3001;
